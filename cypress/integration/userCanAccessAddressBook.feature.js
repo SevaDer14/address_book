e60debf5867bd7b312b8c322761c53f15288d028
sevader14 <vsevolod.deriushkin@gmail.com>
2021-03-23 19:51:41 +0100

    adds second test

--- cypress/integration/userCanAccessAddressBook.feature.js
+++ cypress/integration/userCanAccessAddressBook.feature.js
@@ -1,5 +1,6 @@
 describe("user can access Address Book", () => {
     it("checks if user can access Address Book", () => {
         cy.visit('/')
+        cy.get('h1').should('contain.text', 'Address Book')
     })
 })
